513c539c7d6e676c3f91b871ca8bf21329dad1eb
Jørn Andre Tangen @gorillatron <jornandretangen@gmail.com>
2021-12-03 11:38:29 +0100

    performance tests

--- test/Performance.purs
+++ test/Performance.purs
@@ -11,16 +11,28 @@ import RMRK.Syntax.Parser (parser)
 main :: Effect Unit
 main = do
   let
-    r = range 0 5000
+    stmts = 9
+
+    r = range 0 (20000 / stmts)
   logShow "parsing 20 000 remarks"
   _ <- time "parsing 20 000 remarks"
   for_ r \_ -> do
     let
-      _ = runParser parser "rmrk::BUY::2.0.0::nftid::recipientid"
-
       _ = runParser parser "rmrk::LIST::2.0.0::5105000-0aff6865bed3a66b-VALHELLO-POTION_HEAL-00000001::10000000000"
 
       _ = runParser parser "rmrk::BURN::2.0.0::5105000-0aff6865bed3a66b-VALHELLO-POTION_HEAL-00000001"
+
+      _ = runParser parser "rmrk::BUY::2.0.0::nftid::recipientid"
+
+      _ = runParser parser "rmrk::BUY::2.0.0::nftid"
+
+      _ = runParser parser "rmrk::ACCEPT::2.0.0::nftid::RES::V1i6B"
+
+      _ = runParser parser "rmrk::ACCEPT::2.0.0::nftid::NFT::nftid2"
+
+      _ = runParser parser "rmrk::CHANGEISSUER::2.0.0::base-2345-SYM::somereceiveraddress"
+
+      _ = runParser parser "rmrk::CHANGEISSUER::2.0.0::0aff6865bed3a66b-KANARIA::somereceiveraddress"
 
       _ = runParser parser basejson
     pure unit
