f588b1300d844d6cb47bc68c84b951dd9d446951
Jørn Andre Tangen @gorillatron <jornandretangen@gmail.com>
2021-12-10 15:02:24 +0100

    example

--- src/Main.purs
+++ src/Main.purs
@@ -1,14 +1,3 @@
-# purescript-rmrk-parser
-
-### RMRK 2.0.0 protocol purescript parser
-
-RMRK protocoll parser and primitives in purescript.
-
-![Build & Test](https://github.com/rmrk-team/purescript-rmrk-parser/actions/workflows/build_and_test.yml/badge.svg)
-
-## Quick Example
-
-```purs
 module Main where
 
 import Prelude
@@ -30,24 +19,3 @@ main = do
     Left (ParserError error) -> logShow error
     Right (Tuple (MINT V2 (nftbase) Nothing) "") -> logShow "mint stmt parsed"
     Right _ -> log "only mint supported by this parser"
-
-```
-
-## Spec
-
-Go to the spec repo for full documentation of the RMRK v2 spec.
-[rmrk-team/rmrk-spec/tree/master/standards/rmrk2.0.0](https://github.com/rmrk-team/rmrk-spec/tree/master/standards/rmrk2.0.0)
-
-## Docs
-
-Find the docs at [Pursuit](https://pursuit.purescript.org/packages/purescript-rmrk-parser)
-
-## Goal
-
-Write a dumb protocol parser that parses RMRK strings to strong rich types. Spec of protocol consolidation is not defined or handled in this package.
-
-### Run Tests
-
-```bash
-npm run test
-```
